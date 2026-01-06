58a40de1500a6fddc52890ecf509b6062dc1df99
YashBinjalbhavi <yashbhave0@gmail.com>
2026-01-06 14:43:23 +0530

    python notes

---
+++ 2_datatype.py
@@ -0,0 +1,17 @@
+# datatype is used to recognize what kind of data variable holds.
+
+# in python, datatype is categorise into 3 parts: numeric, boolean and collection
+# in python, there is no limit on data
+
+# in numeric there are 3 data types :int, float and complex
+# in booelan there is one datatype : bool/boolean
+# in collection there are 6 datatypes: string,list,tuple,set,frozenset and dictionary
+
+# Numeric data type
+#int : whole numbers
+#float: decimal numbers
+#complex: the number which is written in the form of a+bj 
+#         where a and b can be int or float and j is imagenery number
+#         j = under root -1
+
+
